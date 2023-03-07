012efc8ddc8de727f49014f66bc41de3adf8d414
r4stl1n <royal.r4stl1n@gmail.com>
2023-03-06 23:49:07 -0800

    Added keymap for horizontal split terminal

--- lua/r4stl1n/remap.lua
+++ lua/r4stl1n/remap.lua
@@ -1,2 +1,3 @@
 vim.keymap.set("n", "<leader>d", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>t", function() vim.cmd('vsplit|term') end)
+vim.keymap.set("n", "<leader>r" , function() vim.cmd('split|term') end)
